3b99c483df090ce98c2d5eb06f81f487d719db12
Rafael Medina <rafamedgar@gmail.com>
2016-07-16 23:30:09 +0200

    Generic functions

--- agents/sarah/actions/wdi.py
+++ agents/sarah/actions/wdi.py
@@ -12,6 +12,135 @@ INDEX_YEAR = 4 # Starts at 1960
 
 FIRST_YEAR = 1960
 
+
+def _get_latest(config, key, country):
+    """Obtain latest value for a given country.
+
+    Args:
+        config (ConfigParser): Information about datafile to use.
+        key (str): Key to search.
+        country (str): Country to search (from parser).
+
+    Returns:
+        Tuple: Boolean, Value (usually float), Error string
+    """
+    # Check datafile
+    datafile = config.get('path')
+
+    if not os.path.isfile(datafile):
+        return False, None, 'No encuentro la base de datos %s' % datafile
+
+
+    # Get country
+    if not country:
+        return False, None, '¿Qué país es ese?'
+
+
+    # Obtain value
+    value = None
+
+    with open(datafile, 'r', encoding='mac_roman', newline='') as f:
+        reader = csv.reader(f)
+
+        # Read line by line
+        for line in reader:
+
+            # Check key
+            if not key in line:
+                continue
+
+            # Check country
+            if line[INDEX_COUNTRY] != country:
+                continue
+
+            # Start from the end and stop when a numerical value is reached
+            for col in reversed(line):
+                if col:
+                    try:
+                        value = float(col)
+                        return True, value, None
+
+                    except:
+                        # Not a number
+                        # pass
+                        return False, None, None
+
+    # Found key
+    # if value:
+    #     return True, value, None
+
+    # Didn't find key
+    return False, None, None
+
+def _get_year(config, key, country, year):
+    """Obtain latest value for a given country.
+
+    Args:
+        config (ConfigParser): Information about datafile to use.
+        key (str): Key to search.
+        country (str): Country to search (from parser).
+        year (int): Year to obtain (from parser).
+
+    Returns:
+        Tuple: Boolean, Value (usually float), Error string
+    """
+    # Check datafile
+    datafile = config.get('path')
+
+    if not os.path.isfile(datafile):
+        return False, None, 'No encuentro la base de datos %s' % datafile
+
+
+    # Get country
+    if not country:
+        return False, None, '¿Qué país es ese?'
+
+    # Get year
+    try:
+        year = int(year)
+
+    except:
+        return False, None, '¿Cuándo dices?'
+
+
+    # Obtain value
+    value = None
+
+    with open(datafile, 'r', encoding='mac_roman', newline='') as f:
+        reader = csv.reader(f)
+
+        # Read line by line
+        for line in reader:
+
+            # Check key
+            if not key in line:
+                continue
+
+            # Check country
+            if line[INDEX_COUNTRY] != country:
+                continue
+
+            # Get specific column
+            col = INDEX_YEAR + (year - 1960)
+
+            try:
+                value = float(line[col])
+                return True, value, None
+
+            except:
+                # Not a number
+                # pass
+                return False, None, None
+
+
+    # Found key
+    # if value:
+    #     return True, value, None
+
+    # Didn't find key
+    return False, None, None
+
+
 def gini(config, parser):
     """Obtain latest GINI for a given country.
 
@@ -19,51 +148,25 @@ def gini(config, parser):
         config (ConfigParser): Information about datafile to use.
         country (str): Country to search (from parser).
     """
-    datafile = config.get('path')
-
-    if not os.path.isfile(datafile):
-        return 'No encuentro la base de datos'
-
-    # Get country
+    key = 'SI.POV.GINI'
     country = parser.get('country')
 
-    if not country:
-        return '¿Qué país es ese?'
+    status, value, errmsg = _get_latest(config, key, country)
 
-
-    # Obtain value
-    value = ''
-
-    with open(datafile, 'r', encoding='mac_roman', newline='') as f:
-        reader = csv.reader(f)
-
-        # Read line by line
-        for line in reader:
-
-            # Check country
-            if line[INDEX_COUNTRY] != country:
-                continue
-
-            # Check GINI
-            if not 'SI.POV.GINI' in line:
-                continue
-
-            # Start from the end and stop when a numerical value is reached
-            for col in reversed(line):
-                if col:
-                    try:
-                        value = float(col) / 100
-
-                    except:
-                        # Not a number
-                        pass
-
-                    break
-
-    if value:
+    if status:
+        # Found value
+        value = value / 100
         return 'El Gini para %s es %.4f' % (country, value)
 
-    return 'No hay valor de Gini para %s' % country
+    # No value
+    if errmsg:
+        # Something happened before finding the key
+        return errmsg
+
+    else:
+        # Didn't find value for the specified country
+        return 'No hay valor de Gini para %s' % country
+
 
 def gini_year(config, parser):
     """Obtain GINI for a given country in a specific year.
@@ -71,57 +174,28 @@ def gini_year(config, parser):
     Args:
         config (ConfigParser): Information about datafile to use.
         country (str): Country to search (from parser).
+        year (int): Year to obtain (from parser).
     """
-    datafile = config.get('path')
-
-    if not os.path.isfile(datafile):
-        return 'No encuentro la base de datos'
-
-    # Get country
+    key = 'SI.POV.GINI'
     country = parser.get('country')
+    year = parser.get('year')
 
-    if not country:
-        return '¿Qué país es ese?'
+    status, value, errmsg = _get_year(config, key, country, year)
 
-    # Get year
-    try:
-        year = int(parser.get('year'))
-
-    except:
-        return '¿Cuándo dices?'
-
-
-    # Obtain value
-    value = ''
-
-    with open(datafile, 'r', encoding='mac_roman', newline='') as f:
-        reader = csv.reader(f)
-
-        # Read line by line
-        for line in reader:
-
-            # Check country
-            if line[INDEX_COUNTRY] != country:
-                continue
-
-            # Check GINI
-            if not 'SI.POV.GINI' in line:
-                continue
-
-            # Get specific column
-            col = INDEX_YEAR + (year - 1960)
-
-            try:
-                value = float(line[col]) / 100
-
-            except:
-                # Not a number
-                pass
-
-    if value:
+    if status:
+        # Found value
+        value = value / 100
         return 'El Gini para %s en %d es %.4f' % (country, year, value)
 
-    return 'No hay valor de Gini para %s en %d' % (country, year)
+    # No value
+    if errmsg:
+        # Something happened before finding the key
+        return errmsg
+
+    else:
+        # Didn't find value for the specified country
+        return 'No hay valor de Gini para %s en %d' % (country, year)
+
 
 def pib(config, parser):
     """Obtain latest PIB for a given country.
@@ -130,51 +204,25 @@ def pib(config, parser):
         config (ConfigParser): Information about datafile to use.
         country (str): Country to search (from parser).
     """
-    datafile = config.get('path')
-
-    if not os.path.isfile(datafile):
-        return 'No encuentro la base de datos'
-
-    # Get country
+    key = 'NY.GDP.MKTP.CD'
     country = parser.get('country')
 
-    if not country:
-        return '¿Qué país es ese?'
+    status, value, errmsg = _get_latest(config, key, country)
 
-
-    # Obtain value
-    value = ''
-
-    with open(datafile, 'r', encoding='mac_roman', newline='') as f:
-        reader = csv.reader(f)
-
-        # Read line by line
-        for line in reader:
-
-            # Check country
-            if line[INDEX_COUNTRY] != country:
-                continue
-
-            # Check PIB
-            if not 'NY.GDP.MKTP.CD' in line:
-                continue
-
-            # Start from the end and stop when a numerical value is reached
-            for col in reversed(line):
-                if col:
-                    try:
-                        value = float(col) / 1000000
-
-                    except:
-                        # Not a number
-                        pass
-
-                    break
-
-    if value:
+    if status:
+        # Found value
+        value = value / 1000000
         return 'El PIB en %s es de %.2f Millones de $' % (country, value)
 
-    return 'No hay PIB en %s' % country
+    # No value
+    if errmsg:
+        # Something happened before finding the key
+        return errmsg
+
+    else:
+        # Didn't find value for the specified country
+        return 'No hay PIB en %s' % country
+
 
 def pib_year(config, parser):
     """Obtain PIB for a given country in a specific year.
@@ -182,57 +230,29 @@ def pib_year(config, parser):
     Args:
         config (ConfigParser): Information about datafile to use.
         country (str): Country to search (from parser).
+        year (int): Year to obtain (from parser).
     """
-    datafile = config.get('path')
-
-    if not os.path.isfile(datafile):
-        return 'No encuentro la base de datos'
-
-    # Get country
+    key = 'NY.GDP.MKTP.CD'
     country = parser.get('country')
+    year = parser.get('year')
 
-    if not country:
-        return '¿Qué país es ese?'
+    status, value, errmsg = _get_year(config, key, country, year)
 
-    # Get year
-    try:
-        year = int(parser.get('year'))
+    if status:
+        # Found value
+        value = value / 1000000
+        return 'El PIB en %s en %d es de %.2f Millones de $' % (
+            country, year, value)
 
-    except:
-        return '¿Cuándo dices?'
+    # No value
+    if errmsg:
+        # Something happened before finding the key
+        return errmsg
 
+    else:
+        # Didn't find value for the specified country
+        return 'No hay PIB en %s en %d' % (country, year)
 
-    # Obtain value
-    value = ''
-
-    with open(datafile, 'r', encoding='mac_roman', newline='') as f:
-        reader = csv.reader(f)
-
-        # Read line by line
-        for line in reader:
-
-            # Check country
-            if line[INDEX_COUNTRY] != country:
-                continue
-
-            # Check PIB
-            if not 'NY.GDP.MKTP.CD' in line:
-                continue
-
-            # Get specific column
-            col = INDEX_YEAR + (year - 1960)
-
-            try:
-                value = float(line[col]) / 1000000
-
-            except:
-                # Not a number
-                pass
-
-    if value:
-        return 'El PIB de %s en %d es de %.2f Millones de $' % (country, year, value)
-
-    return 'No hay PIB de %s en %d' % (country, year)
 
 def pibpc(config, parser):
     """Obtain latest PIBpc for a given country.
@@ -241,51 +261,24 @@ def pibpc(config, parser):
         config (ConfigParser): Information about datafile to use.
         country (str): Country to search (from parser).
     """
-    datafile = config.get('path')
-
-    if not os.path.isfile(datafile):
-        return 'No encuentro la base de datos'
-
-    # Get country
+    key = 'NY.GDP.PCAP.CD'
     country = parser.get('country')
 
-    if not country:
-        return '¿Qué país es ese?'
+    status, value, errmsg = _get_latest(config, key, country)
 
-
-    # Obtain value
-    value = ''
-
-    with open(datafile, 'r', encoding='mac_roman', newline='') as f:
-        reader = csv.reader(f)
-
-        # Read line by line
-        for line in reader:
-
-            # Check country
-            if line[INDEX_COUNTRY] != country:
-                continue
-
-            # Check PIBpc
-            if not 'NY.GDP.PCAP.CD' in line:
-                continue
-
-            # Start from the end and stop when a numerical value is reached
-            for col in reversed(line):
-                if col:
-                    try:
-                        value = float(col)
-
-                    except:
-                        # Not a number
-                        pass
-
-                    break
-
-    if value:
+    if status:
+        # Found value
         return 'El PIB per Cápita de %s es $ %.2f' % (country, value)
 
-    return 'No hay PIB per Cápita de %s' % country
+    # No value
+    if errmsg:
+        # Something happened before finding the key
+        return errmsg
+
+    else:
+        # Didn't find value for the specified country
+        return 'No hay PIB per Cápita de %s' % country
+
 
 def pibpc_year(config, parser):
     """Obtain PIBpc for a given country in a specific year.
@@ -293,57 +286,28 @@ def pibpc_year(config, parser):
     Args:
         config (ConfigParser): Information about datafile to use.
         country (str): Country to search (from parser).
+        year (int): Year to obtain (from parser).
     """
-    datafile = config.get('path')
-
-    if not os.path.isfile(datafile):
-        return 'No encuentro la base de datos'
-
-    # Get country
+    key = 'NY.GDP.PCAP.CD'
     country = parser.get('country')
+    year = parser.get('year')
 
-    if not country:
-        return '¿Qué país es ese?'
+    status, value, errmsg = _get_year(config, key, country, year)
 
-    # Get year
-    try:
-        year = int(parser.get('year'))
+    if status:
+        # Found value
+        return 'El PIB per Cápita de %s en %d es $ %.2f' % (
+            country, year, value)
 
-    except:
-        return '¿Cuándo dices?'
+    # No value
+    if errmsg:
+        # Something happened before finding the key
+        return errmsg
 
+    else:
+        # Didn't find value for the specified country
+        return 'No hay PIB per Cápita para %s en %d' % (country, year)
 
-    # Obtain value
-    value = ''
-
-    with open(datafile, 'r', encoding='mac_roman', newline='') as f:
-        reader = csv.reader(f)
-
-        # Read line by line
-        for line in reader:
-
-            # Check country
-            if line[INDEX_COUNTRY] != country:
-                continue
-
-            # Check PIBpc
-            if not 'NY.GDP.PCAP.CD' in line:
-                continue
-
-            # Get specific column
-            col = INDEX_YEAR + (year - 1960)
-
-            try:
-                value = float(line[col])
-
-            except:
-                # Not a number
-                pass
-
-    if value:
-        return 'El PIB per Cápita de %s en %d es $ %.2f' % (country, year, value)
-
-    return 'No hay PIB per Cápita para %s en %d' % (country, year)
 
 def desemp(config, parser):
     """Obtain latest Unenployment for a given country.
@@ -352,107 +316,49 @@ def desemp(config, parser):
         config (ConfigParser): Information about datafile to use.
         country (str): Country to search (from parser).
     """
-    datafile = config.get('path')
-
-    if not os.path.isfile(datafile):
-        return 'No encuentro la base de datos'
-
-    # Get country
+    key = 'SL.UEM.TOTL.NE.ZS'
     country = parser.get('country')
 
-    if not country:
-        return '¿Qué país es ese?'
+    status, value, errmsg = _get_latest(config, key, country)
+
+    if status:
+        # Found value
+        return 'El Desempleo en %s es del %.4f %%' % (country, value)
+
+    # No value
+    if errmsg:
+        # Something happened before finding the key
+        return errmsg
+
+    else:
+        # Didn't find value for the specified country
+        return 'No hay tasa de Desempleo en %s' % country
 
 
-    # Obtain value
-    value = ''
-
-    with open(datafile, 'r', encoding='mac_roman', newline='') as f:
-        reader = csv.reader(f)
-
-        # Read line by line
-        for line in reader:
-
-            # Check country
-            if line[INDEX_COUNTRY] != country:
-                continue
-
-            # Check Desemp
-            if not 'SL.UEM.TOTL.NE.ZS' in line:
-                continue
-
-            # Start from the end and stop when a numerical value is reached
-            for col in reversed(line):
-                if col:
-                    try:
-                        value = float(col)
-
-                    except:
-                        # Not a number
-                        pass
-
-                    break
-
-    if value:
-        return 'El Desempleo en %s es del %.4f %' % (country, value)
-
-    return 'No hay tasa de Desempleo en %s' % country
-
-def gini_year(config, parser):
+def desemp_year(config, parser):
     """Obtain Unemployment for a given country in a specific year.
 
     Args:
         config (ConfigParser): Information about datafile to use.
         country (str): Country to search (from parser).
+        year (int): Year to obtain (from parser).
     """
-    datafile = config.get('path')
-
-    if not os.path.isfile(datafile):
-        return 'No encuentro la base de datos'
-
-    # Get country
+    key = 'SL.UEM.TOTL.NE.ZS'
     country = parser.get('country')
+    year = parser.get('year')
 
-    if not country:
-        return '¿Qué país es ese?'
+    status, value, errmsg = _get_year(config, key, country, year)
 
-    # Get year
-    try:
-        year = int(parser.get('year'))
+    if status:
+        # Found value
+        return 'El Desempleo en %s en %d es del %.4f %%' % (
+            country, year, value)
 
-    except:
-        return '¿Cuándo dices?'
+    # No value
+    if errmsg:
+        # Something happened before finding the key
+        return errmsg
 
-
-    # Obtain value
-    value = ''
-
-    with open(datafile, 'r', encoding='mac_roman', newline='') as f:
-        reader = csv.reader(f)
-
-        # Read line by line
-        for line in reader:
-
-            # Check country
-            if line[INDEX_COUNTRY] != country:
-                continue
-
-            # Check Desemp
-            if not 'SL.UEM.TOTL.NE.ZS' in line:
-                continue
-
-            # Get specific column
-            col = INDEX_YEAR + (year - 1960)
-
-            try:
-                value = float(line[col])
-
-            except:
-                # Not a number
-                pass
-
-    if value:
-        return 'El Desempleo en %s en %d es del %.2f %' % (country, year, value)
-
-    return 'No hay tasa de Desempleo en %s en %d' % (country, year)
-
+    else:
+        # Didn't find value for the specified country
+        return 'No hay tasa de Desempleo en %s en %d' % (country, year)
